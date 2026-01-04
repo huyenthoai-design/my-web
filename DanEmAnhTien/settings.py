f2a4daa0faf0fe41fb1e018b59f5e572bcc33c67
huyenthoai <0945799112thoai@gmail.com>
2026-01-04 21:23:46 +0700

    initial commit

--- DanEmAnhTien/settings.py
+++ DanEmAnhTien/settings.py
@@ -99,7 +99,10 @@ WSGI_APPLICATION = 'DanEmAnhTien.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-   
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
