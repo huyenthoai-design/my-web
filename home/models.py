7e52ce134d0ac80b2b48f70e369bfbfa7c680b27
huyenthoai <0945799112thoai@gmail.com>
2026-01-06 18:55:21 +0700

    initial commit

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from mongoengine import Document, StringField, EmailField
+
+class UserAccount(Document):
+    meta = {'collection': 'users'} # Dữ liệu sẽ lưu vào bảng 'users'
+    
+    username = StringField(required=True, unique=True)
+    password = StringField(required=True) # Đây là nơi lưu mật khẩu đã mã hóa
+    full_name = StringField()
+    email = EmailField()
+
+    def __str__(self):
+        return self.username
